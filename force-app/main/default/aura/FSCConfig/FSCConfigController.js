5410383c3daaa6203dcf60222ef1fd38d09363b7
Mohan Chinnappan <mohan.chinnappan.n@gmail.com>
2019-06-30 13:42:30 -0400

    demo update

---
+++ force-app/main/default/aura/FSCConfig/FSCConfigController.js
@@ -0,0 +1,12 @@
+({
+	doInit : function(component, event, helper) {
+      /*
+        var saveBtn = document.querySelector('.uiModal--medium.forceModal.DESKTOP.uiModal.forceModal.open.active>div.panel.slds-modal.slds-fade-in-open>div.modal-container.slds-modal__container>div.modal-footer.slds-modal__footer>div.slds-grid.slds-grid--align-spread.FinServWizardFooter>div.slds-order--3.slds-align-middle.slds-size--3-of-7>button');
+        saveBtn.addEventListener("click", function() {
+            alert('SAVE Called');
+        });
+      */
+        console.log('doInit');
+    }
+         
+})
