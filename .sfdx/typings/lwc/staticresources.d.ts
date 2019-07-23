a35986c9ce13756177f5b8b992b5d18ea7340896
Mohan Chinnappan <mohan.chinnappan.n@gmail.com>
2019-07-22 21:02:26 -0400

    remove Change Owner button added

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,52 @@
+declare module "@salesforce/resourceUrl/FSCConfig" {
+    var FSCConfig: string;
+    export default FSCConfig;
+}
+declare module "@salesforce/resourceUrl/FSCConfigAddGroup" {
+    var FSCConfigAddGroup: string;
+    export default FSCConfigAddGroup;
+}
+declare module "@salesforce/resourceUrl/FSCConfigAddRelLabel" {
+    var FSCConfigAddRelLabel: string;
+    export default FSCConfigAddRelLabel;
+}
+declare module "@salesforce/resourceUrl/FSCConfigChangeOwnerButton" {
+    var FSCConfigChangeOwnerButton: string;
+    export default FSCConfigChangeOwnerButton;
+}
+declare module "@salesforce/resourceUrl/FSCConfigDropdownMenu" {
+    var FSCConfigDropdownMenu: string;
+    export default FSCConfigDropdownMenu;
+}
+declare module "@salesforce/resourceUrl/FSCConfigMakeRelMapRO" {
+    var FSCConfigMakeRelMapRO: string;
+    export default FSCConfigMakeRelMapRO;
+}
+declare module "@salesforce/resourceUrl/FSCConfigMenu" {
+    var FSCConfigMenu: string;
+    export default FSCConfigMenu;
+}
+declare module "@salesforce/resourceUrl/FSCConfigPopup" {
+    var FSCConfigPopup: string;
+    export default FSCConfigPopup;
+}
+declare module "@salesforce/resourceUrl/FSCConfigRelContact" {
+    var FSCConfigRelContact: string;
+    export default FSCConfigRelContact;
+}
+declare module "@salesforce/resourceUrl/FSCConfigRemoveMember" {
+    var FSCConfigRemoveMember: string;
+    export default FSCConfigRemoveMember;
+}
+declare module "@salesforce/resourceUrl/FSCConfigSaveButton" {
+    var FSCConfigSaveButton: string;
+    export default FSCConfigSaveButton;
+}
+declare module "@salesforce/resourceUrl/FSCConfigSpinner" {
+    var FSCConfigSpinner: string;
+    export default FSCConfigSpinner;
+}
+declare module "@salesforce/resourceUrl/FSCConfigWizBackBtn" {
+    var FSCConfigWizBackBtn: string;
+    export default FSCConfigWizBackBtn;
+}
