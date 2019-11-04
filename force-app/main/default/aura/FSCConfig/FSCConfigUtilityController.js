0c82d00913f17ddf7168e36b77781bd0ff07c1cf
Mohan Chinnappan <mohan.chinnappan.n@gmail.com>
2019-11-04 15:37:15 -0500

    dx faq added

---
+++ force-app/main/default/aura/FSCConfig/FSCConfigUtilityController.js
@@ -0,0 +1,14 @@
+({
+	doInit : function(component, event, helper) {
+      /*
+        var saveBtn = document.querySelector('.uiModal--medium.forceModal.DESKTOP.uiModal.forceModal.open.active>div.panel.slds-modal.slds-fade-in-open>div.modal-container.slds-modal__container>div.modal-footer.slds-modal__footer>div.slds-grid.slds-grid--align-spread.FinServWizardFooter>div.slds-order--3.slds-align-middle.slds-size--3-of-7>button');
+        saveBtn.addEventListener("click", function() {
+            alert('SAVE Called');
+        });
+      */
+        // in PROD window.console.log does nop
+        console.log = function () { }; 
+        // console.log('doInit');
+    }
+         
+})
