9f61aae8e9e6350912fc15cab14b3e4631481786
Mohan Chinnappan <mohan.chinnappan.n@gmail.com>
2020-03-02 20:00:36 -0500

    model update

---
+++ model/add-header.sh
@@ -0,0 +1,15 @@
+## 1
+cat header.csv fsc-model-1a2.csv  > fsc-model-1a2_.csv; mv fsc-model-1a2_.csv fsc-model-1a2.csv
+
+cat header.csv fsc-model-1b1.csv  > fsc-model-1b1_.csv; mv fsc-model-1b1_.csv fsc-model-1b1.csv
+cat header.csv fsc-model-1b2.csv  > fsc-model-1b2_.csv; mv fsc-model-1b2_.csv fsc-model-1b2.csv
+
+## 2
+cat header.csv fsc-model-2a1.csv  > fsc-model-2a1_.csv; mv fsc-model-2a1_.csv fsc-model-2a1.csv
+cat header.csv fsc-model-2a2.csv  > fsc-model-2a2_.csv; mv fsc-model-2a2_.csv fsc-model-2a2.csv
+
+
+cat header.csv fsc-model-2b1.csv  > fsc-model-2b1_.csv; mv fsc-model-2b1_.csv fsc-model-2b1.csv
+cat header.csv fsc-model-2b2.csv  > fsc-model-2b2_.csv; mv fsc-model-2b2_.csv fsc-model-2b2.csv
+
+
